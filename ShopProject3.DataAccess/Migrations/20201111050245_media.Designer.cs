4e5f00204d4f9cd3272557b04a5ff557d6672658
Dimitris Kougioumtzis <dimkoug@gmail.com>
2020-11-11 07:04:03 +0200

    Add migration for media table

--- ShopProject3.DataAccess/Migrations/20201111050245_media.Designer.cs
+++ ShopProject3.DataAccess/Migrations/20201111050245_media.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopProject3.DataAccess;
 
 namespace ShopProject3.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201111050245_media")]
+    partial class media
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ShopProject3.DataAccess/Migrations/20201111050245_media.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopProject3.DataAccess.Migrations
+{
+    public partial class media : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductMedia_Documents_DocumentsEntityId",
+                table: "ProductMedia");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductMedia_MediaEntity_MediaId",
+                table: "ProductMedia");
+
+            migrationBuilder.DropTable(
+                name: "Documents");
+
+            migrationBuilder.DropTable(
+                name: "ShoppingCartItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductMedia_DocumentsEntityId",
+                table: "ProductMedia");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MediaEntity",
+                table: "MediaEntity");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentsEntityId",
+                table: "ProductMedia");
+
+            migrationBuilder.RenameTable(
+                name: "MediaEntity",
+                newName: "Media");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "OffersDetail",
+                type: "decimal(19,10)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OffersId",
+                table: "OffersDetail",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Offers",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Published",
+                table: "Offers",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Deleted",
+                table: "Offers",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Offers",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Media",
+                table: "Media",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OffersDetail_OffersId",
+                table: "OffersDetail",
+                column: "OffersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OffersDetail_Offers_OffersId",
+                table: "OffersDetail",
+                column: "OffersId",
+                principalTable: "Offers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductMedia_Media_MediaId",
+                table: "ProductMedia",
+                column: "MediaId",
+                principalTable: "Media",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OffersDetail_Offers_OffersId",
+                table: "OffersDetail");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductMedia_Media_MediaId",
+                table: "ProductMedia");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OffersDetail_OffersId",
+                table: "OffersDetail");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Media",
+                table: "Media");
+
+            migrationBuilder.DropColumn(
+                name: "OffersId",
+                table: "OffersDetail");
+
+            migrationBuilder.RenameTable(
+                name: "Media",
+                newName: "MediaEntity");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DocumentsEntityId",
+                table: "ProductMedia",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "OffersDetail",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(19,10)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Offers",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Published",
+                table: "Offers",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Deleted",
+                table: "Offers",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Offers",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MediaEntity",
+                table: "MediaEntity",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Documents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Deleted = table.Column<bool>(type: "bit", nullable: false),
+                    FilePath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    Published = table.Column<bool>(type: "bit", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShoppingCartItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    SessionCartId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCartItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShoppingCartItem_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductMedia_DocumentsEntityId",
+                table: "ProductMedia",
+                column: "DocumentsEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCartItem_ProductId",
+                table: "ShoppingCartItem",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductMedia_Documents_DocumentsEntityId",
+                table: "ProductMedia",
+                column: "DocumentsEntityId",
+                principalTable: "Documents",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductMedia_MediaEntity_MediaId",
+                table: "ProductMedia",
+                column: "MediaId",
+                principalTable: "MediaEntity",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
